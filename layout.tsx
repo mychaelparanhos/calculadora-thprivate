bd058931ee7d35eac17e5dab76dbf078ba15cf51
mychaelparanhos <mychael.paranhos@gmail.com>
2025-09-22 11:16:02 -0300

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = { title: 'Calculadora TH Private' };
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-BR">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
