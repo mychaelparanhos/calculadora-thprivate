721f3ef2bb15b583cb9a0c79946bdd7cc3e9af9d
mychaelparanhos <mychael.paranhos@gmail.com>
2025-09-22 11:02:55 -0300

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: 'Calculadora TH Private' };
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="pt-BR">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
-  );
-}
